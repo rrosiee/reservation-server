bb3537e689032f295bfd7b7bcad98d4dcdb0f6a1
rrosiee <offbeat1020@naver.com>
2025-03-05 23:28:25 +0900

    feat: 확정 여부에 인덱싱

--- apps/reservation/models/index.py
+++ apps/reservation/models/index.py
@@ -9,7 +9,7 @@ class Reservation(BaseModel):
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     applicant_count = models.IntegerField()
-    is_confirmed = models.BooleanField(default=False)
+    is_confirmed = models.BooleanField(default=False, db_index=True)
 
     # FK
     reserver_user = models.ForeignKey(
@@ -17,7 +17,7 @@ class Reservation(BaseModel):
         verbose_name="예약자",
         on_delete=models.SET_NULL,
         null=True,
-        related_name="reservations",
+        related_name="reservations"
     )
 
     class Meta:
